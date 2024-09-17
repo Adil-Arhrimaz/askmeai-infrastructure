9d206b067919d6a3fd06fbed0613a30736b6fc5a
Adil Arhrimaz <adilarhrimaz@gmail.com>
2024-09-17 23:53:37 +0100

    add terraform state

--- providers.tf
+++ providers.tf
@@ -7,6 +7,14 @@ terraform {
       version = ">= 5.46"
     }
   }
+
+  backend "s3" {
+    bucket         = "terraform-state"
+    key            = "state/${terraform.workspace}/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
